82bff51b275445706536163c0ed9e5c40313a5cc
Tatiana <efimenko-ts@mail.ru>
2022-07-03 16:12:49 +0300

    fix: исправлено подключение модуля validator

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,16 +1,10 @@
 const { Joi, celebrate } = require('celebrate');
-const { validator } = require('validator');
+const validator = require('validator');
 
 const validateUpdateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
-    // email: Joi.string().custom((value, helpers) => {
-    //   if (validator.isEmail(value)) {
-    //     return value;
-    //   }
-    //   return helpers.message('Email введен некорректно');
-    // }),
   }),
 });
 
@@ -36,39 +30,29 @@ const validateMovie = celebrate({
     duration: Joi.number().required(),
     year: Joi.number().required(),
     description: Joi.string().required(),
-    image: Joi.string().required(),
-    trailerLink: Joi.string().required(),
-
-    // image: Joi.string().required().custom((value, helpers) => {
-    //   if (validator.isURL(value, { protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true })) {
-    //     return value;
-    //   }
-    //   return helpers.message('Ссылка введена некорректно');
-    // }),
-    // trailerLink: Joi.string().required().custom((value, helpers) => {
-    //   if (validator.isURL(value, { protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true })) {
-    //     return value;
-    //   }
-    //   return helpers.message('Ссылка введена некорректно');
-    // }),
+    // image: Joi.string().required(),
+    image: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value, { protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true })) {
+        return value;
+      }
+      return helpers.message('Ссылка введена некорректно');
+    }),
+    // trailerLink: Joi.string().required(),
+    trailerLink: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value, { protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true })) {
+        return value;
+      }
+      return helpers.message('Ссылка введена некорректно');
+    }),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
-    thumbnail: Joi.string().required().uri(),
-    // thumbnail: Joi.string().required().custom((value, helpers) => {
-    //   if (validator.isURL(value)) {
-    //     return value;
-    //   }
-    //   return helpers.message('Ссылка введена некорректно');
-    // }),
-    // thumbnail: Joi.string().required().custom((value) => {
-    //   if (validator.isURL(value)) {
-    //     return value;
-    //   }
-    //   const notUrlError = new Error('Введенные данные не соответствуют формату ссылки');
-    //   notUrlError.statusCode = 400;
-    //   // return next(error);
-    //   return notUrlError;
-    // }),
+    // thumbnail: Joi.string().required().uri(),
+    thumbnail: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Ссылка введена некорректно');
+    }),
     movieId: Joi.number().required(),
   }),
 });
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,19 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+// const { celebrate, Joi } = require('celebrate');
 const { movieRouter } = require('./movies');
 const { userRouter } = require('./users');
 const { isAuthorized } = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
-const { validateUserInfo, validateAuth } = require('../middlewares/validation');
-
-// router.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30),
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required(),
-//   }),
-// }), createUser);
+const { validateUserInfo, validateMovie, validateAuth } = require('../middlewares/validation');
 
 router.post('/signup', validateUserInfo, createUser);
-// router.post('/signup', createUser);
 router.post('/signin', validateAuth, login);
-
-// router.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required(),
-//   }),
-// }), login);
 
 router.use(isAuthorized);
 
 router.use('/users', userRouter);
-// app.use('/movies', validateMovie, movieRouter);
-router.use('/movies', movieRouter);
+router.use('/movies', validateMovie, movieRouter);
+// router.use('/movies', movieRouter);
 
 router.use((req, res, next) => {
   const error = new Error('Страница не существует');
